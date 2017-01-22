5015339be4d6629d60a0dfb77918acabee5c2f63
Eric Barch <ericb@ericbarch.com>
2017-01-22 10:33:17 -0500

    Use serial0 for tty.

--- pi-control/index.js
+++ pi-control/index.js
@@ -19,7 +19,7 @@ for (let i = 0; i < 4; i ++) {
 }
 
 // init serialport
-var port = new SerialPort("/dev/ttyS0", {
+var port = new SerialPort("/dev/serial0", {
   baudRate: 115200,
   parser: SerialPort.parsers.byteDelimiter([13,10])
 });
