e5d520796f8c1594470dd5a98775b9eb5f9683fb
Eric Barch <ericb@ericbarch.com>
2017-01-28 06:58:39 -0500

    Implement push notifications on cycle end and time tracking.

--- pi-control/index.js
+++ pi-control/index.js
@@ -8,6 +8,31 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var SerialPort = require("serialport");
 var Gpio = require('onoff').Gpio;
+var push = require('pushover-notifications');
+
+// pushover key for sending push notifications
+const PUSHOVER_USER_KEY = '';
+const PUSHOVER_TOKEN = '';
+
+const MACHINE_NAME = 'Thing 3';
+
+
+
+
+
+
+// create pushover API obj
+var p = new push({
+  user: PUSHOVER_USER_KEY,
+  token: PUSHOVER_TOKEN
+});
+
+// which machine codes to stop the timer and send a push notification
+const machineEndCodes = ['m00', 'm01', 'm02', 'm30'];
+
+let machineHasPaused = true;
+let cycleStartTime = 0;
+let totalRunTimeSeconds = 0;
 
 // we will use output pins BCM17/18/27/22 (RPi Physical Pins 11/12/13/15)
 const RELAY_PINS = [17, 18, 27, 22];
@@ -24,8 +49,40 @@ var port = new SerialPort("/dev/serial0", {
   parser: SerialPort.parsers.readline('\n')
 });
 
+function sendPushNotification (cycleTimeSeconds, lineString) {
+  let hhMmSsCycle = (new Date(cycleTimeSeconds * 1000)).toISOString().substr(11, 8);
+  let hhMmSsTotal = (new Date(totalRunTimeSeconds * 1000)).toISOString().substr(11, 8);
+
+  var msg = {
+    title: MACHINE_NAME + ': ' + lineString,
+    message: MACHINE_NAME + ' finished run in ' + hhMmSsCycle + '. Accumulated run time today: ' + hhMmSsTotal + '.'  // required
+  };
+
+  p.send(msg, function (err, result) {
+    if (err) {
+      console.log('Push notification send err: ' + err);
+    }
+  });
+}
+
 port.on('data', function (data) {
   console.log('Serial Data: ' + data);
+
+  let lineLowerCase = data.trim().toLowerCase();
+
+  if (machineEndCodes.indexOf(lineLowerCase) !== -1 && !machineHasPaused) {
+    let cycleTimeSeconds = Math.floor(Date.now() / 1000) - cycleStartTime;
+    machineHasPaused = true;
+
+    // keep a running count of total run time (unless reset)
+    totalRunTimeSeconds += cycleTimeSeconds;
+
+    sendPushNotification(cycleTimeSeconds, data.trim());
+  } else if (machineHasPaused) {
+    machineHasPaused = false;
+    cycleStartTime = Math.floor(Date.now() / 1000);
+  }
+
   io.sockets.emit('console', data);
 });
 
@@ -93,6 +150,20 @@ io.on('connection', function(socket){
     }
   });
 });
+
+// reset total run time var and reschedule reset for midnight
+function resetTotalRunTime () {
+  totalRunTimeSeconds = 0;
+
+  var midnight = new Date();
+  midnight.setHours(24,0,0,0);
+  var now = new Date();
+  var msToMidnight = midnight - now;
+  setTimeout(resetTotalRunTime, msToMidnight);
+}
+
+// schedule timer to reset total run time
+resetTotalRunTime();
 
 // fire up the http server
 server.listen(3000);
